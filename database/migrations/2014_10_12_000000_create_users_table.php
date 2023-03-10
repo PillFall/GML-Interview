9c0850f8a42807ae810a7d495684d7868254aa88
Joar Esteban Buitrago Carrillo <jebuitragoc@unal.edu.co>
2023-03-10 18:21:50 -0500

    🗃 Add all the required data in the users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 100);
+            $table->string('surname', 100);
+            $table->string('identifier')->unique();
+            $table->string('country');
+            $table->string('mobile', 10);
+            $table->string('address', 180);
+            $table->string('email', 150)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
