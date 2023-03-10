413956c0db1cef21b63ab35589a819db230dee43
Joar Esteban Buitrago Carrillo <jebuitragoc@unal.edu.co>
2023-03-10 17:28:16 -0500

    💄 Create basic UI scaffolding

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,15 +8,14 @@ import './bootstrap';
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 createInertiaApp({
-    resolve: (name) => {
-        const pages = import.meta.glob('./pages/**/*.vue');
-        return pages[`./pages/${name}.vue`];
-    },
+    resolve: (name) => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob('./pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .mount(el);
     },
+    progress: null,
 });
