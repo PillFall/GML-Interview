3c2d423c99d3a9f8c91e547714e4e4518b72044c
Joar Esteban Buitrago Carrillo <jebuitragoc@unal.edu.co>
2023-03-11 03:16:12 -0500

    📝 Add explanation about slow response

--- resources/js/pages/home.vue
+++ resources/js/pages/home.vue
@@ -4,6 +4,7 @@
         h1 Hola, Soy Joar
         p Esta es la aplicación que desarrolle como parte del requisito tecnico para aplicar a un cargo en GML.
         p Si estas viendo esto, significa que la aplicación se esta ejecutando de manera exitosa. Puedes avanzar y seguir analizando la aplicación utilizando el menu superior. Asi como acceder a mi repositorio de GitHub.
+        p Es posible que las cargas iniciales del indice de pacientes sean un poco lentas. Esto sucede porque aun no se ha generado la cache de los elementos que se van a mostrar.
         p Muchas Gracias por esta oportunidad.
 
     .level
